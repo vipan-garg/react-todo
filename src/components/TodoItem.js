a8374ddd74dc73b86f25f30a3115dfbe36d6fec8
Vipan Garg <vipan.garg@kiwitech.com>
2021-01-07 23:35:23 +0530

    first commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,26 @@
+import React from "react"
+class TodoItem extends React.Component {
+    render() {
+        const completedStyle = {
+            fontStyle: "italic",
+            color: "#d35e0f",
+            opacity: 0.4,
+            textDecoration: "line-through",
+          }
+        return <li className="todo-item">
+            <input
+                type="checkbox"
+                checked={this.props.todo.completed}
+                onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+            />
+            <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+                Delete
+            </button>
+            <span style={this.props.todo.completed ? completedStyle : null}>
+                {this.props.todo.title}
+            </span>
+            </li>
+    }
+}
+
+export default TodoItem
